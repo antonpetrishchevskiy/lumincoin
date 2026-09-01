1956301586d27c7643acf761f641baa4c9080129
antonpetrishchevskiy <121608533+antonpetrishchevskiy@users.noreply.github.com>
2026-09-01 12:08:34 +0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,51 +1,12 @@
-import {Router} from "./src/router.js";
+import {Router} from "./router.js";
+import "./styles/styles.scss";
 
-class App {
-    constructor() {
-        new Router();
-    }
+const startApp = () => {
+    new Router();
+};
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', startApp, {once: true});
+} else {
+    startApp();
 }
-
-(new App());
-
-const ctx = document.getElementById('myChart');
-
-// круги на воде
-new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue', ''],
-        datasets: [{
-            data: [12, 19, 3, 5, 2, 0],
-            backgroundColor: [
-                'rgb(218,53,68)',
-                'rgb(251,125,20)',
-                'rgb(253,191,7)',
-                'rgb(32,199,150)',
-                'rgb(13,109,251)',
-                'rgba(13,109,251, 0)',
-            ]
-        }]
-    },
-});
-
-
-const ctx2 = document.getElementById('myChart2');
-
-new Chart(ctx2, {
-    type: 'pie',
-    data: {
-        labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue', ''],
-        datasets: [{
-            data: [12, 19, 3, 5, 2, 0],
-            backgroundColor: [
-                'rgb(218,53,68)',
-                'rgb(251,125,20)',
-                'rgb(253,191,7)',
-                'rgb(32,199,150)',
-                'rgb(13,109,251)',
-                'rgba(13,109,251, 0)',
-            ]
-        }]
-    },
-});
